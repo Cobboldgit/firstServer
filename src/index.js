c9401606e443a50c483731dfa5a4889f65adfbb7
Cobboldgit <augustinecobbold6@gmail.com>
2022-03-22 21:18:32 +0000

    add html file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const path = require('path')
 
 const app = express();
 
-app.use("/About", (req, res) => {
-  res.send("About");
-});
+const handleHomeRoute = (request, response) => {
+  const homePath = path.join(__dirname, "public", "pages", "Home.html");
+  response.sendFile(homePath);
+};
 
-app.use("/Login", (req, res) => {
-  res.send("Login");
-});
+const handleAboutRoute = (request, response) => {
+  const aboutPath = path.join(__dirname, "public", "pages", "About.html");
+  response.sendFile(aboutPath);
+};
 
-app.use("/", (req, res) => {
-  res.send("Hola amigos, you are in the HOME PAGE");
-});
+app.get("/about", handleAboutRoute);
+app.get("/", handleHomeRoute);
 
 app.listen("5003", () => {
   console.log("server is running on port 5003");
